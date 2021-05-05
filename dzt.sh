36ec7640262756edefafd8121c851da1a0ff66a1
kintrosec <80190862+kintrosec@users.noreply.github.com>
2021-05-05 23:28:51 +0530

    Update dzt.sh

--- dzt.sh
+++ dzt.sh
@@ -13,7 +13,7 @@ echo " "
 read -p 'Enter Domain: ' domain
 echo " "
 echo " "
-(host -t mx $domain | awk '{print $7}'; host -t ns $domain | awk '{print $4}'; host -t A $domain | awk '{print $4}') > dn.txt
+host -t ns $domain | awk '{print $4}' > dn.txt
 
 
 
